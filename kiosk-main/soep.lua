c9ca472c733534099465d9dded8775f0a9d76698
JonVer403 <jcverhoeven@outlook.com>
2026-02-17 10:18:24 +0100

    afrekenebebeebe

--- kiosk-main/soep.lua
+++ kiosk-main/soep.lua
@@ -69,7 +69,7 @@ function scene:create( event )
     end
     
     -- Cart button
-    local cartBtn = display.newRect(sceneGroup, display.contentWidth * 0.7, display.contentHeight - 50, 100, 40)
+    local cartBtn = display.newRect(sceneGroup, display.contentWidth * 0.5, display.contentHeight - 50, 100, 40)
     cartBtn:setFillColor(0.2, 0.7, 0.2)
     local cartText = display.newText(sceneGroup, "Afrekenen", display.contentWidth * 0.7, display.contentHeight - 50, native.systemFont, 18)
     cartText:setFillColor(1, 1, 1)
